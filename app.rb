3b780e4c8de8bb36b76df953f15c5b5e923e7a71
davreed4793 <david.reed3@kellogg.northwestern.edu>
2020-06-08 21:24:27 +0000

    Commit everything

--- app.rb
+++ app.rb
@@ -1,6 +1,7 @@
 # Set up for the application and database. DO NOT CHANGE. #############################
 require "sinatra"                                                                     #
 require "sinatra/reloader" if development?                                            #
+require "sinatra/cookies"  
 require "sequel"                                                                      #
 require "logger"                                                                      #
 require "twilio-ruby"                                                                 #
@@ -15,5 +16,87 @@ before { puts; puts "--------------- NEW REQUEST ---------------"; puts }       
 after { puts; }                                                                       #
 #######################################################################################
 
-# events_table = DB.from(:events)
-# rsvps_table = DB.from(:rsvps)
+stadiums_table = DB.from(:stadiums)
+reviews_table = DB.from(:reviews)
+users_table = DB.from(:users)
+
+
+before do
+    @current_user = users_table.where(:id => session[:user_id]).to_a[0]
+    puts @current_user.inspect
+end
+
+get "/" do 
+    @stadiums = stadiums_table.all
+    puts @stadiums.inspect 
+    view "stadiums"
+end 
+
+get "/stadiums/:id" do 
+    @stadium = stadiums_table.where(:id => params["id"]).to_a[0]
+    puts @stadium.inspect
+    @reviews = reviews_table.where(:stadiumid => params["id"]).to_a
+    puts @reviews.inspect 
+
+    results = Geocoder.search(@stadium[:location])
+    @lat_long = results.first.coordinates.join(",")
+    puts @lat_long.inspect
+    view "stadium"
+end 
+
+get "/stadiums/:id/reviews/new" do 
+    @stadium = stadiums_table.where(:id => params["id"]).to_a[0]
+    view "newreview"
+end 
+
+get "/stadiums/:id/reviews/create" do 
+    @stadium = stadiums_table.where(:id => params["id"]).to_a[0]
+
+    puts params.inspect
+    reviews_table.insert(:stadiumid => params["id"],
+                        :rating => params["rating"],
+                        :username => @current_user[:username],
+                        :userid => @current_user[:id],
+                        :review => params["comments"])
+    view "reviewsubmitted"
+end 
+
+get "/users/new" do
+    view "newuser"
+end
+
+post "/users/create" do
+    users_table.insert(:username => params["name"],
+                       :password => BCrypt::Password.create(params["password"]))
+    puts params.inspect
+    view "createuser"
+end
+
+get "/logins/new" do
+    view "newlogin"
+end
+
+post "/logins/create" do 
+    puts params 
+    username_entered = params["name"]
+    password_entered = params["password"]
+    user = users_table.where(:username => username_entered).to_a[0]
+    if user 
+        if BCrypt::Password.new(user[:password]) == password_entered
+        session[:user_id] = user[:id]
+    view "loginsuccess"
+    else 
+    view "createloginfail"
+    end 
+    else view "createloginfail"
+    end 
+end 
+
+# Logout
+get "/logout" do
+    session[:user_id] = nil 
+    view "/logout"
+end
+
+
+ 
--- createdb.rb
+++ createdb.rb
@@ -5,31 +5,41 @@ DB = Sequel.connect(connection_string)                                          
 #######################################################################################
 
 # Database schema - this should reflect your domain model
-DB.create_table! :events do
+DB.create_table! :stadiums do
   primary_key :id
-  String :title
-  String :description, text: true
-  String :date
+  String :stadiumname 
+  String :avgrating
   String :location
 end
-DB.create_table! :rsvps do
+DB.create_table! :reviews do
   primary_key :id
-  foreign_key :event_id
-  Boolean :going
-  String :name
-  String :email
-  String :comments, text: true
+  foreign_key :stadiumid
+  foreign_key :userid
+  String :username
+  String :rating
+  String :review, text: true
+end
+DB.create_table! :users do
+  primary_key :id
+  String :username
+  String :password
 end
 
 # Insert initial (seed) data
-events_table = DB.from(:events)
+stadiums_table = DB.from(:stadiums)
+reviews_table = DB.from(:reviews)
+users_table = DB.from(:users)
 
-events_table.insert(title: "Bacon Burger Taco Fest", 
-                    description: "Here we go again bacon burger taco fans, another Bacon Burger Taco Fest is here!",
-                    date: "June 21",
-                    location: "Kellogg Global Hub")
+stadiums_table.insert(stadiumname: "Raymond James Stadium", 
+                    avgrating: "9.4",
+                    location: "4201 N Dale Mabry Hwy, Tampa, FL 33607")
 
-events_table.insert(title: "Kaleapolooza", 
-                    description: "If you're into nutrition and vitamins and stuff, this is the event for you.",
-                    date: "July 4",
-                    location: "Nowhere")
+stadiums_table.insert(stadiumname: "Tropicana Field", 
+                    avgrating: "4.2",
+                    location: "1 Tropicana Dr., St. Petersburg, FL 33705")
+
+reviews_table.insert(stadiumid: "1",
+                    username: "davreed", 
+                    rating: "2",
+                    review: "Testing comment section")
+
